50907df8944b2570393be4f7f2e9c3af9b6afcd1
Heshani Thennakoon <heshanithennakoon118@gmail.com>
2025-12-09 22:51:35 +0530

    Update the Login page

--- LectureLoginApp/obj/Debug/net10.0/LectureLoginApp.AssemblyInfo.cs
+++ LectureLoginApp/obj/Debug/net10.0/LectureLoginApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LectureLoginApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f081b64788c5450b31dddbb50b3726ad054de20a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b82b43062b6829f1f2152788d4e2bfe94541fb97")]
 [assembly: System.Reflection.AssemblyProductAttribute("LectureLoginApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LectureLoginApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
